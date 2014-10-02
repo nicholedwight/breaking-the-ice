30e54fd90aac9c3b44fdff2f39684832785425f4
Nichole Dwight <nicholedwight@gmail.com>
2014-10-02 20:52:18 +0100

    Nuking IDs and doing some animation stuff. Aww yiss

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var toggle = document.getElementById('aboutToggle');
 var slider = document.querySelector('.slider');
 var homeSection = document.querySelector('.home');
+var nav = document.getElementById('home-nav');
 
 toggle.addEventListener('click', toggleSlider, false);
 
@@ -9,11 +10,13 @@ function toggleSlider(){
         slider.classList.remove('opened');
         slider.classList.add('closed');
         homeSection.classList.remove('mini');
+        nav.classList.remove('compressed')
         console.log('closing');
     } else {
         slider.classList.remove('closed');
         slider.classList.add('opened');
         homeSection.classList.add('mini');
+        nav.classList.add('compressed');
         console.log('opening');
     }
 }
