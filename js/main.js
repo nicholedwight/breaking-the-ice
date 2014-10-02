e15da7276113c53cda3289ebdaf7483cea7359b9
Nichole Dwight <nicholedwight@icanhascheezburger.local>
2014-10-02 20:19:26 +0100

    Slide up now works

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var toggle = document.getElementById('aboutToggle');
+var slider = document.querySelector('.slider');
+var homeSection = document.querySelector('.home');
+
+toggle.addEventListener('click', toggleSlider, false);
+
+function toggleSlider(){
+    if (slider.classList.contains('opened')) {
+        slider.classList.remove('opened');
+        slider.classList.add('closed');
+        homeSection.classList.remove('mini');
+        console.log('closing');
+    } else {
+        slider.classList.remove('closed');
+        slider.classList.add('opened');
+        homeSection.classList.add('mini');
+        console.log('opening');
+    }
+}
