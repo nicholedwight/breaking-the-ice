375da88e995158463dc9e9dd357c64f78be3c36c
Nichole Dwight <nicholedwight@gmail.com>
2014-10-02 21:13:38 +0100

    Nav transitions

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 var toggle = document.getElementById('aboutToggle');
 var slider = document.querySelector('.slider');
 var homeSection = document.querySelector('.home');
-var nav = document.getElementById('home-nav');
 
 toggle.addEventListener('click', toggleSlider, false);
 
@@ -9,14 +8,12 @@ function toggleSlider(){
     if (slider.classList.contains('opened')) {
         slider.classList.remove('opened');
         slider.classList.add('closed');
-        homeSection.classList.remove('mini');
-        nav.classList.remove('compressed')
+        homeSection.classList.remove('compressed');
         console.log('closing');
     } else {
         slider.classList.remove('closed');
         slider.classList.add('opened');
-        homeSection.classList.add('mini');
-        nav.classList.add('compressed');
+        homeSection.classList.add('compressed');
         console.log('opening');
     }
 }
