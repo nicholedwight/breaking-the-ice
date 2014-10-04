89f959d7fa3c656a6e5845c327f8895bfa43a679
Nichole Dwight <nicholedwight@gmail.com>
2014-10-04 20:02:11 +0100

    Added google maps api, not customised

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 // var mainNav = document.querySelector('.home-nav');
 // mainNav.classList.add('hidden');
 
-var toggle = document.getElementById('toggle');
-var slideUp = document.querySelector('.slide-up');
-var homeSection = document.querySelector('.home');
-
-toggle.addEventListener('click', toggleSlider, false);
-
-
-function toggleSlider(){
-    if (slideUp.classList.contains('opened')) {
-        slideUp.classList.remove('opened');
-        slideUp.classList.add('closed');
-        homeSection.classList.remove('compressed');
-        toggle.classList.remove('current');
-        console.log('closing');
-    } else {
-        slideUp.classList.remove('closed');
-        slideUp.classList.add('opened');
-        homeSection.classList.add('compressed');
-        toggle.classList.add('current');
-        console.log('opening');
-    }
-}
+// var toggle = document.getElementById('toggle');
+// var slideUp = document.querySelector('.slide-up');
+// var homeSection = document.querySelector('.home');
+//
+// toggle.addEventListener('click', toggleSlider, false);
+//
+//
+// function toggleSlider(){
+//     if (slideUp.classList.contains('opened')) {
+//         slideUp.classList.remove('opened');
+//         slideUp.classList.add('closed');
+//         homeSection.classList.remove('compressed');
+//         toggle.classList.remove('current');
+//         console.log('closing');
+//     } else {
+//         slideUp.classList.remove('closed');
+//         slideUp.classList.add('opened');
+//         homeSection.classList.add('compressed');
+//         toggle.classList.add('current');
+//         console.log('opening');
+//     }
+// }
 
 // var mobileNav = document.getElementById('collapsibleNav');
 // mobileNav.addEventListener('click', toggleNav, false)
