37e4e9ee1d1f61ee110f0e6d5fa4f5c08d0f7dad
Nichole Dwight <nicholedwight@gmail.com>
2014-10-04 17:42:55 +0100

    changed slider class to slideUp cuz sense bro

--- js/main.js
+++ js/main.js
@@ -2,21 +2,21 @@ var mainNav = document.querySelector('.home-nav');
 mainNav.classList.add('hidden');
 
 var toggle = document.getElementById('aboutToggle');
-var slider = document.querySelector('.slider');
+var slideUp = document.querySelector('.slide-up');
 var homeSection = document.querySelector('.home');
 
 toggle.addEventListener('click', toggleSlider, false);
 
 function toggleSlider(){
-    if (slider.classList.contains('opened')) {
-        slider.classList.remove('opened');
-        slider.classList.add('closed');
+    if (slideUp.classList.contains('opened')) {
+        slideUp.classList.remove('opened');
+        slideUp.classList.add('closed');
         homeSection.classList.remove('compressed');
         toggle.classList.remove('current');
         console.log('closing');
     } else {
-        slider.classList.remove('closed');
-        slider.classList.add('opened');
+        slideUp.classList.remove('closed');
+        slideUp.classList.add('opened');
         homeSection.classList.add('compressed');
         toggle.classList.add('current');
         console.log('opening');
