d5badbe79b75a8c4fb0f3a40f7a95059b96f3a10
Nichole Dwight <nicholedwight@gmail.com>
2014-10-05 13:16:42 +0100

    custom map markers and map positioning

--- js/main.js
+++ js/main.js
@@ -34,3 +34,43 @@
 //       mainNav.classList.add('hidden');
 //   }
 // }
+
+
+// Code for multiple markers modified from http://theoryapp.com/google-maps-with-multiple-markers-and-infowindow/
+// Code for map was from Google Maps API
+var LocationData = [
+    [64.008696376988884, -22.564784111440713, "<div class='map-info'>First Host</div>" ],
+    [63.988296376988884, -22.545784111440713, "<div class='map-info'>Second Host</div>" ]
+];
+function initialize(){
+    var mapOptions = {
+        center: new google.maps.LatLng(64.0167, -22.5667),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    var bounds = new google.maps.LatLngBounds();
+    var infowindow = new google.maps.InfoWindow();
+
+
+    for (var i in LocationData) {
+        var p = LocationData[i];
+        var latlng = new google.maps.LatLng(p[0], p[1]);
+        var image = { url: 'img/mapMarker.png'};
+
+        var marker = new google.maps.Marker({
+            position: latlng,
+            map: map,
+            icon: image,
+            title: p[2]
+        });
+
+        google.maps.event.addListener(marker, 'click', (function(mm, tt) {
+          return function() {
+              infowindow.setContent(tt);
+              infowindow.open(map, mm);
+          }
+        })(marker, p[2]));
+    }
+}
+google.maps.event.addDomListener(window, 'load', initialize);
