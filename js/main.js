f7bdfebae5dd63016d3e9f20d462806551f953ef
Nichole Dwight <nicholedwight@gmail.com>
2014-10-05 18:50:09 +0100

    mouseover and out colour changes and click infowindows now working

--- js/main.js
+++ js/main.js
@@ -38,96 +38,70 @@
 
 // Code for multiple markers modified from http://theoryapp.com/google-maps-with-multiple-markers-and-infowindow/
 // Code for map was from Google Maps API
-// var LocationData = [
-//     [64.008696376988884, -22.564784111440713, "<div class='map-info'>First Host</div>" ],
-//     [63.988296376988884, -22.545784111440713, "<div class='map-info'>Second Host</div>" ]
-// ];
-// function initialize(){
-//     var mapOptions = {
-//         center: new google.maps.LatLng(64.0167, -22.5667),
-//         zoom: 10,
-//         mapTypeId: google.maps.MapTypeId.ROADMAP
-//     };
-//     var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-//     var bounds = new google.maps.LatLngBounds();
-//     var infowindow = new google.maps.InfoWindow();
-//     var currentHover = document.querySelector('.host-wrapper');
-//     var icon = { url: 'img/mapMarker.png'};
-//     var iconCurrent = { url: 'img/mapCurrent.png'};
-
-    // var marker1 = new google.maps.Marker({
-    //     position: new google.maps.LatLng(64.008696376988884, -22.564784111440713),
-    //     map: map,
-    //     icon: icon,
-    //     infoWindow: {
-    //       content: "<div class='map-info'>First Host</div>"
-    //     }
-    // });
-    // var contentString = "<div class='map-info'>First Host</div>";
-    // google.maps.event.addListener(marker1, 'click', function() {
-    //   infowindow.open(map,marker1);
-    // });
-    // google.maps.event.addListener(marker1, 'mouseover', function () {
-    //   marker1.setIcon(iconCurrent);
-    // });
-    // google.maps.event.addListener(marker1, 'mouseout', function () {
-    //   marker1.setIcon(icon);
-    // });
-// }
 
 var locations = [
   ["<div class='map-info'>First Host</div>", 64.008696376988884, -22.564784111440713],
   ["<div class='map-info'>Second Host</div>", 63.988296376988884, -22.545784111440713]
   ];
 
-  function initialize() {
+function initialize() {
 
-    var mapOptions = {
-      center: new google.maps.LatLng(64.0167, -22.5667),
-      zoom: 10,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+  var mapOptions = {
+    center: new google.maps.LatLng(64.0167, -22.5667),
+    zoom: 10,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
 
-    };
-    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  };
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-    setMarkers(map,locations)
+  setMarkers(map,locations)
+
+}
+
+function setMarkers(map,locations){
+
+    var marker, i
+
+    for (i = 0; i < locations.length; i++) {
+
+       var host = locations[i][0]
+       var lat = locations[i][1]
+       var long = locations[i][2]
+       var icon = { url: 'img/mapMarker.png'}
+       var iconCurrent = { url: 'img/mapCurrent.png'}
+
+       latlngset = new google.maps.LatLng(lat, long);
+
+        var marker = new google.maps.Marker({
+                map: map, title: host, icon: icon, position: latlngset
+              });
+              map.setCenter(marker.getPosition())
+
+
+        var content = host
+
+        var infowindow = new google.maps.InfoWindow()
+
+      google.maps.event.addListener(marker,'mouseover', (function(marker,content,infowindow){
+              return function() {
+                 marker.setIcon(iconCurrent);
+              };
+          })(marker,content,infowindow));
+      google.maps.event.addListener(marker,'mouseout', (function(marker,content,infowindow){
+              return function() {
+                 marker.setIcon(icon);
+              };
+          })(marker,content,infowindow));
+      google.maps.event.addListener(marker,'click', (function(marker,content,infowindow){
+              return function() {
+                 infowindow.setContent(content);
+                 infowindow.open(map,marker);
+              };
+          })(marker,content,infowindow));
 
   }
 
-
-
-  function setMarkers(map,locations){
-
-      var marker, i
-
-      for (i = 0; i < locations.length; i++) {
-
-         var host = locations[i][0]
-         var lat = locations[i][1]
-         var long = locations[i][2]
-         var icon = { url: 'img/mapMarker.png'}
-
-         latlngset = new google.maps.LatLng(lat, long);
-
-          var marker = new google.maps.Marker({
-                  map: map, title: host, icon: icon, position: latlngset
-                });
-                map.setCenter(marker.getPosition())
-
-
-          var content = host
-
-          var infowindow = new google.maps.InfoWindow()
-
-        google.maps.event.addListener(marker,'click', (function(marker,content,infowindow){
-                return function() {
-                   infowindow.setContent(content);
-                   infowindow.open(map,marker);
-                };
-            })(marker,content,infowindow));
-
-    }
-  }
+}
 
 
 google.maps.event.addDomListener(window, 'load', initialize);
