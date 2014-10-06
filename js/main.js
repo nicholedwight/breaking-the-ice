b243b78ea297180ff1c06fe32a2a5d04562dd257
Nichole Dwight <nicholedwight@gmail.com>
2014-10-06 09:31:00 +0100

    individual host family info slides now and is accessible with no javascript

--- js/main.js
+++ js/main.js
@@ -1,42 +1,22 @@
-// var mainNav = document.querySelector('.home-nav');
-// mainNav.classList.add('hidden');
+//This following code shows and hides individual host family details, they slide up and down on the main hosts page
+$(document).ready(function() {
+  $('.toggle').click(function(){
+    $('#hosts').slideUp();
+    $('.slide-hidden').css("display", "block");
+    console.log('opening');
+    });
+});
 
-// var toggle = document.getElementById('toggle');
-// var slideUp = document.querySelector('.slide-up');
-// var homeSection = document.querySelector('.home');
-//
-// toggle.addEventListener('click', toggleSlider, false);
-//
-//
-// function toggleSlider(){
-//     if (slideUp.classList.contains('opened')) {
-//         slideUp.classList.remove('opened');
-//         slideUp.classList.add('closed');
-//         homeSection.classList.remove('compressed');
-//         toggle.classList.remove('current');
-//         console.log('closing');
-//     } else {
-//         slideUp.classList.remove('closed');
-//         slideUp.classList.add('opened');
-//         homeSection.classList.add('compressed');
-//         toggle.classList.add('current');
-//         console.log('opening');
-//     }
-// }
-
-// var mobileNav = document.getElementById('collapsibleNav');
-// mobileNav.addEventListener('click', toggleNav, false)
-//
-// function toggleNav() {
-//   if (mainNav.classList.contains('hidden')) {
-//       mainNav.classList.remove('hidden');
-//   } else {
-//       mainNav.classList.add('hidden');
-//   }
-// }
+$(document).ready(function() {
+$('.toggleBack').click(function(){
+  $('#hosts').slideDown();
+  $('.slide-hidden').css("display", "none");
+  console.log('closing');
+});
+});
 
 
-// Code for multiple markers modified from http://theoryapp.com/google-maps-with-multiple-markers-and-infowindow/
+// Code for multiple markers was modified from http://theoryapp.com/google-maps-with-multiple-markers-and-infowindow/
 // Code for map was from Google Maps API
 
 var locations = [
