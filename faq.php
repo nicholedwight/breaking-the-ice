af7b8b809d177ba21ef2f2f4f817a638622c67e6
Nichole Dwight <nicholedwight@gmail.com>
2014-10-18 08:14:18 +0100

    Fixed h1,h2 and p stylings

--- faq.php
+++ faq.php
@@ -4,13 +4,14 @@
   <main class="secondary_wrapper" role="main">
     <a id="skip"> </a>
     <?php include('inc/faq_nav.php'); ?>
-    <article id-"get_here">
-      <h1>How to get to Iceland</h1>
+    <h1>Practical Info</h1>
+    <article>
+      <h2>Where's Iceland?</h2>
       <p>Typewriter drinking vinegar narwhal cardigan. Forage seitan chia craft beer tattooed YOLO viral, bicycle rights kale chips. Tilde roof party DIY Truffaut, gluten-free pickled wayfarers beard readymade banh mi locavore Austin chia selfies. Mumblecore YOLO mlkshk salvia, single-origin coffee fashion axe leggings selfies swag sartorial Bushwick Pinterest Truffaut trust fund Schlitz. Put a bird on it PBR&B YOLO bespoke cliche, Thundercats swag mlkshk locavore polaroid next level fap. Messenger bag cardigan authentic, paleo plaid Truffaut slow-carb. Hoodie plaid McSweeney's letterpress polaroid, Thundercats Truffaut locavore jean shorts next level deep v Godard farm-to-table occupy.</p>
     </article>
-    <article id="weather">
-      <h1>Weather in Iceland</h1>
-      <p>Typewriter drinking vinegar narwhal cardigan. Forage seitan chia craft beer tattooed YOLO viral, bicycle rights kale chips. Tilde roof party DIY Truffaut, gluten-free pickled wayfarers beard readymade banh mi locavore Austin chia selfies. Mumblecore YOLO mlkshk salvia, single-origin coffee fashion axe leggings selfies swag sartorial Bushwick Pinterest Truffaut trust fund Schlitz. Put a bird on it PBR&B YOLO bespoke cliche, Thundercats swag mlkshk locavore polaroid next level fap. Messenger bag cardigan authentic, paleo plaid Truffaut slow-carb. Hoodie plaid McSweeney's letterpress polaroid, Thundercats Truffaut locavore jean shorts next level deep v Godard farm-to-table occupy.</p>
+    <article>
+      <h2>How long is it daylight for in Iceland?</h2>
+      <p>Some of our visitors who join us in the Summer ask when it will get dark, they're told "sometime in August". The sun hardly sets at all in the Summer around the southern half of the country, if you stay north of Reykjavík it's daylight 24/7. If you joing us in the Winter, however, do expect only a few, 3-4, hours of daylight. Spring and Fall daylight hours will be more normal.</p>
     </article>
 
   </main>
