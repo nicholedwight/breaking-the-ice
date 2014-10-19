4700d722b93c561d45e2cf6b0589d3b4a9d6dd55
Nichole Dwight <nicholedwight@gmail.com>
2014-10-19 12:03:44 +0100

    Added contact form

--- contact.php
+++ contact.php
@@ -3,9 +3,24 @@
 ?>
 <main class="secondary_wrapper" role="main">
   <a id="skip"> </a>
-  <article>
-    <h1>Contact</h1>
-    <p>Typewriter drinking vinegar narwhal cardigan. Forage seitan chia craft beer tattooed YOLO viral, bicycle rights kale chips. Tilde roof party DIY Truffaut, gluten-free pickled wayfarers beard readymade banh mi locavore Austin chia selfies. Mumblecore YOLO mlkshk salvia, single-origin coffee fashion axe leggings selfies swag sartorial Bushwick Pinterest Truffaut trust fund Schlitz. Put a bird on it PBR&B YOLO bespoke cliche, Thundercats swag mlkshk locavore polaroid next level fap. Messenger bag cardigan authentic, paleo plaid Truffaut slow-carb. Hoodie plaid McSweeney's letterpress polaroid, Thundercats Truffaut locavore jean shorts next level deep v Godard farm-to-table occupy.</p>
-  </article>
+  <div class="contact_form">
+
+  </div>
+  <div class="contact_sidebar">
+    <aside class="contact_details">
+      <h2>Email</h2>
+      <p>
+        <a href="mailto:nicholedwight@gmail.com">
+          nicholedwight@gmail.com
+        </a>
+      </p>
+      <p>
+        <a href="https://twitter.com/breakingiceland" target="_blank">
+          <i class="icon icon_twitter"></i>
+          @breakingiceland
+        </a>
+      </p>
+    </aside>
+  </div>
 </main>
 <?php include('inc/footer.php'); ?>
