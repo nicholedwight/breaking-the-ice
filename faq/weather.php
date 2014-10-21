157041254bedd3ddc676838ec08a205d4e084e79
Nichole Dwight <nicholedwight@gmail.com>
2014-10-21 07:09:12 +0100

    removed Galleria

--- faq/weather.php
+++ faq/weather.php
@@ -2,12 +2,6 @@
   include('../inc/header.php');
 ?>
 <main class="secondary_wrapper" role="main">
-  <div class="galleria">
-    <img src="/project1/assets/img/jegtnes.jpg">
-    <img src="/project1/assets/img/jegtnes.jpg">
-    <img src="/project1/assets/img/jegtnes.jpg">
-  </div>
-
   <div class="inner_wrapper">
     <a id="skip"> </a>
     <!-- <img src="http://placekitten.com/g/500/200"> -->
@@ -22,7 +16,3 @@
   </div>
 </main>
 <?php include('../inc/footer.php'); ?>
-<script src="/project1/assets/js/lib/galleria-1.4.2.min.js"></script>
-<script>  Galleria.loadTheme('/project1/assets/galleria/themes/classic/galleria.classic.min.js');
-  Galleria.run('.galleria');
-</script>
