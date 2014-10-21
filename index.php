641db065baa727198c7a804fc669163b0be76976
Nichole Dwight <nicholedwight@gmail.com>
2014-10-21 14:39:21 +0100

    redesigning homepage

--- index.php
+++ index.php
@@ -7,14 +7,28 @@
   </section>
   <main class="main_wrapper cf" role="main">
     <a id="skip"> </a>
-    <h1>What We Do</h1>
+    <div class="main_content">
+      <div class="content_item">
+        <h2><a href="#">About</a></h2>
+        <p>Typewriter drinking vinegar narwhal cardigan. Forage seitan chia craft beer tattooed YOLO viral, bicycle rights kale chips. Tilde roof party DIY Truffaut, gluten-free pickled wayfarers beard readymade banh mi locavore Austin chia selfies.</p>
+      </div>
+      <div class="content_item">
+        <h2><a href="#">Hosts</a></h2>
+        <p>Typewriter drinking vinegar narwhal cardigan. Forage seitan chia craft beer tattooed YOLO viral, bicycle rights kale chips. Tilde roof party DIY Truffaut, gluten-free pickled wayfarers beard readymade banh mi locavore Austin chia selfies.</p>
+      </div>
+      <div class="content_item">
+        <h2><a href="#">Contact</a></h2>
+        <p>Typewriter drinking vinegar narwhal cardigan. Forage seitan chia craft beer tattooed YOLO viral, bicycle rights kale chips. Tilde roof party DIY Truffaut, gluten-free pickled wayfarers beard readymade banh mi locavore Austin chia selfies.</p>
+      </div>
+    </div>
+    <!-- <h1>What We Do</h1>
     <article class="l_main_content left">
       <p>Typewriter drinking vinegar narwhal cardigan. Forage seitan chia craft beer tattooed YOLO viral, bicycle rights kale chips. Tilde roof party DIY Truffaut, gluten-free pickled wayfarers beard readymade banh mi locavore Austin chia selfies. Mumblecore YOLO mlkshk salvia, single-origin coffee fashion axe leggings selfies swag sartorial Bushwick Pinterest Truffaut trust fund Schlitz. Put a bird on it PBR&B YOLO bespoke cliche, Thundercats swag mlkshk locavore polaroid next level fap. Messenger bag cardigan authentic, paleo plaid Truffaut slow-carb. Hoodie plaid McSweeney's letterpress polaroid, Thundercats Truffaut locavore jean shorts next level deep v Godard farm-to-table occupy.</p>
     </article>
     <aside class="right">
       <h2>See what people are doing in Iceland</h2>
       <div id="instafeed"></div>
-    </aside>
+    </aside> -->
   </main>
 <?php include('inc/footer.php'); ?>
 <script type="text/javascript">
