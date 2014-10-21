55a6daadf7c583c90b1cd27d49b2922aba0a0e45
Nichole Dwight <nicholedwight@gmail.com>
2014-10-21 06:19:57 +0100

    Changed forecast colour and width

--- faq/weather.php
+++ faq/weather.php
@@ -9,7 +9,9 @@
     <article>
       <h1>Weather in Iceland</h1>
       <p>Typewriter drinking vinegar narwhal cardigan. Forage seitan chia craft beer tattooed YOLO viral, bicycle rights kale chips. Tilde roof party DIY Truffaut, gluten-free pickled wayfarers beard readymade banh mi locavore Austin chia selfies. Mumblecore YOLO mlkshk salvia, single-origin coffee fashion axe leggings selfies swag sartorial Bushwick Pinterest Truffaut trust fund Schlitz. Put a bird on it PBR&B YOLO bespoke cliche, Thundercats swag mlkshk locavore polaroid next level fap. Messenger bag cardigan authentic, paleo plaid Truffaut slow-carb. Hoodie plaid McSweeney's letterpress polaroid, Thundercats Truffaut locavore jean shorts next level deep v Godard farm-to-table occupy.</p>
-      <iframe id="forecast_embed" type="text/html" frameborder="0" height="245" width="100%" src="http://forecast.io/embed/#lat=64.1474&lon=-21.9340&name=Reykjavík&units=uk"> </iframe> 
+      <div class="forecast">
+        <iframe id="forecast_embed" type="text/html" frameborder="0" height="245" width="100%" src="http://forecast.io/embed/#lat=64.1474&lon=-21.9340&name=Reykjavík&units=uk&color=#00CD9B"> </iframe>
+      </div>
     </article>
   </div>
 </main>
