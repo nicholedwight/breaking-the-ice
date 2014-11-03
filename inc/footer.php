2135332d77149421c6a5a5f8d260dd0ae7197d09
Nichole Dwight <nicholedwight@gmail.com>
2014-11-03 08:57:20 +0000

    added frosted background on host cards

--- inc/footer.php
+++ inc/footer.php
@@ -18,6 +18,7 @@
 <script src="/project1/assets/js/lib/jquery-1.11.0.min.js"></script>
 <script src="/project1/assets/js/lib/instafeed.min.js"></script>
 <script src="/project1/assets/js/lib/lightbox.min.js"></script>
+<script src="/project1/assets/js/lib/blur.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-smoove/0.2.6/jquery.smoove.min.js"></script><script src="/project1/assets/js/main.js"></script>
 <noscript>
 <link rel="stylesheet" type="text/css" href="/assets/css/nojs.css"><!--css styles for when javascript is disabled-->
