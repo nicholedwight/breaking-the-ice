d888799165f151dc0ec89d89d0f4e98d07ee4351
Nichole Dwight <nicholedwight@gmail.com>
2014-11-03 18:11:21 +0000

    added on formmail, trying to get thisPage variable working, showing as undefined

--- contact.php
+++ contact.php
@@ -6,6 +6,71 @@
   <div class="contact_form">
 
   </div>
-  <h1>Things will be here soon.</h1>
+  <form class="contact_form" action="/project1/inc/FormMail.php" method="POST" name="breaking_the_ice_form">
+		<span class="required">* Required</span>
+		<ul>
+			<li>
+				<label for="name">Name:</label>
+				<input type="text" name="name" id="name" required>
+			</li>
+			<li>
+				<label for="email">Email:</label>
+				<input type="email" name="email" id="email" placeholder="Enter a valid email address" onChange="emailvalidation(this,'The email given is not valid');" required>
+			</li>
+			<li>
+				<label for="age">How old are you?</label>
+				<select name="age" id="age" required>
+					<option selected="selected"> </option>
+					<optgroup label="Teenager">
+						<option>13-15</option>
+						<option>16-19</option>
+					</optgroup>
+					<optgroup label="Adult">
+						<option>20-25</option>
+						<option>25-30</option>
+						<option>30+</option>
+					</optgroup>
+				</select>
+			</li>
+			<li>
+				<label for="continent">Where are you from?</label>
+				<select name="continent" id="continent">
+					<option> </option>
+					<optgroup label="Americas">
+						<option>North America</option>
+						<option>South America</option>
+					</optgroup>
+					<optgroup label="Eurasia">
+						<option>Europe</option>
+						<option>Asia</option>
+					</optgroup>
+						<option>Africa</option>
+						<option>Australia</option>
+						<option>Antarctica</option>
+			</select>
+			</li>
+			<li>
+				<label for="message">Message:</label>
+				<textarea name="message" id="message" rows="6" cols="40" required></textarea>
+			</li>
+		</ul>
+		<button class="submit" type="submit">Submit</button>
+	</form>
 </main>
+<script>
+		// Below script found at echoecho.com, used to validate the email input.
+		function emailvalidation(entered, alertbox) {
+		// E-mail Validation by Henrik Petersen / NetKontoret
+		// Explained at www.echoecho.com/jsforms.htm
+		// Please do not remove this line and the two lines above.
+		with (entered) {
+			apos=value.indexOf("@");
+			dotpos=value.lastIndexOf(".");
+			lastpos=value.length-1;
+			if (apos<1 || dotpos-apos<2 || lastpos-dotpos>3 || lastpos-dotpos<2)
+			{if (alertbox) {alert(alertbox);} return false;}
+			else {return true;}
+			}
+		}
+	</script>
 <?php include('inc/footer.php'); ?>
---
+++ inc/FormMail.php
@@ -0,0 +1,124 @@
+<?PHP
+$thiPage = "contactResponse";
+/*
+FormMail.php
+The program is in two parts:
+the first part receives the values form your Form and assigns them to variables.
+An email is then sent to the specified address.
+The second part creates a simple html response page.
+
+Note that a php script is essentially just anordinary html script with some php statements
+embedded within the code using <php  and  ?>  as the start and end php tags.
+
+This program as it is expects that
+1.  you have written a Form with  'action=FormMail.php method=post'
+2.   your Form contains  3 fields 'name', 'email', and 'message'
+	(You can, if you are careful, amend this program to deal with more fields)
+3.  your Form contains  a hidden field named 'subject' which conatains the phrase
+you wish to appear in the 'Subject' line of the email that is sent to you
+
+ To use this program as it is you *must* change the next line after these comments
+	"you@uwe.ac.uk" must be replaced with your email address
+*/
+
+$YourEmail = "nicholedwight@gmail.com";
+
+/*
+To use the program as it is you do not need to change anything more.
+Comments are included to explain what is happening
+(and to note places where you could edit the program.)
+
+The next lines receive the field values from your Form and assign them
+to $variables which are used later on in the program.
+You could create more variables to handle
+more Form fields - or edit the statements to reflect the names in your Form
+The syntax is $variable=$_POST[fieldname];
+*/
+
+$name=$_POST['name'];
+$email=$_POST['email'];
+$message=$_POST['message'];
+
+/*
+The next lines build your email
+(You have already assigned a value to 'YourEmail' above.)
+ */
+
+$sendto="$YourEmail";
+$headers= "MIME-Version: 1.0\r\n";
+$headers= "Content-type: text/html; ";
+$headers= "charset=iso-8859-1\r\n";
+$headers= "From: $email";
+// Build the email body text
+  $emailcontent = "
+
+Name: $name
+Email: $email
+Message:
+$message
+"
+/* and the email is sent  later - after we have started the html page
+so that we can include an appropriate message  within the page */
+?>
+
+<!--  HTML PAGE
+Except for the inclusion of php variables and the 'mail' statement
+the following is straightforward html code.
+You can add more html code but it should be outside the <php .. ?> tags.
+-->
+  <?php
+  include('header.php'); ?>
+<html>
+<head>
+<title>Breaking the Ice | Contact Response</title>
+<link rel="stylesheet" type="text/css" href="/project1/assets/css/main.css">
+</head>
+<body>
+<h2>Thank you for your email!</h2>
+<?php
+// Send out the email or output an error message
+if (mail($sendto, $emailcontent, $headers)) {
+?>
+  	<p>&nbsp;</p>
+	<p><b>Thank You <?php echo $name; ?></b> an email has been sent to the address you gave.</p>
+  	<p>You sent the following:</p>
+		<table border="0" align="center" cellpadding="6">
+		<tr>
+		<td>Name: </td><td><b><?php echo $name; ?> </b> </td>
+		</tr>
+		<tr>
+		<td>Email: </td><td> <b><?php echo $email; ?> </b></td>
+		</tr>
+		<tr>
+		<td>Message: </td><td><b><?php echo $message; ?> </b> </td>
+		</tr>
+		</table>
+ 	<p>We will be in touch as soon as possible.</p>
+<?php
+	}
+
+else { // email send has been unsuccessful.
+?>
+  	<p>&nbsp;</p>
+	<p><b>Sorry <?php echo $name; ?></b></p>
+	<p>For some reason the form submission has not been successful.</p>
+ 	<p>You tried to send the following:</p>
+		<table border="0" align="center" cellpadding="6">
+		<tr>
+		<td>Name: </td><td><b><?php echo $name; ?> </b> </td>
+		</tr>
+		<tr>
+		<td>Email: </td><td> <b><?php echo $email; ?> </b></td>
+		</tr>
+		<tr>
+		<td>Message: </td><td><b><?php echo $message; ?> </b> </td>
+		</tr>
+		</table>
+
+
+	<p> Please try again.</p>
+<?php
+	}
+?>
+</body>
+</html>
