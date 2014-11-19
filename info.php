5a122775ab031c713d1745eadeda38b994521b4e
Nichole Dwight <nicholedwight@gmail.com>
2014-11-19 18:35:54 +0000

    adding info content and images

--- info.php
+++ info.php
@@ -8,19 +8,24 @@
       <h1>Practical Info</h1>
       <article>
         <h2>Where's Iceland?</h2>
-        <p>Typewriter drinking vinegar narwhal cardigan. Forage seitan chia craft beer tattooed YOLO viral, bicycle rights kale chips. Tilde roof party DIY Truffaut, gluten-free pickled wayfarers beard readymade banh mi locavore Austin chia selfies. Mumblecore YOLO mlkshk salvia, single-origin coffee fashion axe leggings selfies swag sartorial Bushwick Pinterest Truffaut trust fund Schlitz. Put a bird on it PBR&B YOLO bespoke cliche, Thundercats swag mlkshk locavore polaroid next level fap. Messenger bag cardigan authentic, paleo plaid Truffaut slow-carb. Hoodie plaid McSweeney's letterpress polaroid, Thundercats Truffaut locavore jean shorts next level deep v Godard farm-to-table occupy.</p>
+        <p>Iceland is a North Atlantic island and the westernmost country in Europe, midway between North America and mainland Europe. It lies about 800 km northwest of Scotland and 970 km west of Norway, and its northern coast is just below the Arctic Circle. From London, Iceland is the same distance as Athens. It is also the same distance from New York to Iceland as from New York to Los Angeles. Reykjavík is the world’s northernmost capital city.</p>
         <h2>How long is it daylight for in Iceland?</h2>
         <p>Some of our visitors who join us in the Summer ask when it will get dark, they're told "sometime in August". The sun hardly sets at all in the Summer around the southern half of the country, if you stay north of Reykjavík it's daylight 24/7. If you joing us in the Winter, however, do expect only a few, 3-4, hours of daylight; come hunt the lights with us! The Northern Lights are a spectacular part of Iceland in the Winter and many of our hosts enjoy taking our guests out to see them. Spring and Fall daylight hours will be more normal.</p>
+        <div class="image_column">
+          <img src="/project1/assets/img/bonfire.jpg" alt="Bonfire by the beach">
+          <img src="/project1/assets/img/blanketgirl.jpg" alt="Enjoying the scenery">
+          <img src="/project1/assets/img/icelandhills.jpg" alt="Enjoying the scenery">
+        </div>
       </article>
 
       <article id="how-to-get-here">
         <h1>How to get to Iceland</h1>
-        <p>Typewriter drinking vinegar narwhal cardigan. Forage seitan chia craft beer tattooed YOLO viral, bicycle rights kale chips. Tilde roof party DIY Truffaut, gluten-free pickled wayfarers beard readymade banh mi locavore Austin chia selfies. Mumblecore YOLO mlkshk salvia, single-origin coffee fashion axe leggings selfies swag sartorial Bushwick Pinterest Truffaut trust fund Schlitz. Put a bird on it PBR&B YOLO bespoke cliche, Thundercats swag mlkshk locavore polaroid next level fap. Messenger bag cardigan authentic, paleo plaid Truffaut slow-carb. Hoodie plaid McSweeney's letterpress polaroid, Thundercats Truffaut locavore jean shorts next level deep v Godard farm-to-table occupy.</p>
+        <p>Frequent flights operate to Iceland from main cities in Europe and North America and many gateways are served daily. Flight times to Europe are two to four hours.</p>
       </article>
 
       <article id="weather">
         <h1>Weather in Iceland</h1>
-        <p>Typewriter drinking vinegar narwhal cardigan. Forage seitan chia craft beer tattooed YOLO viral, bicycle rights kale chips. Tilde roof party DIY Truffaut, gluten-free pickled wayfarers beard readymade banh mi locavore Austin chia selfies. Mumblecore YOLO mlkshk salvia, single-origin coffee fashion axe leggings selfies swag sartorial Bushwick Pinterest Truffaut trust fund Schlitz. Put a bird on it PBR&B YOLO bespoke cliche, Thundercats swag mlkshk locavore polaroid next level fap. Messenger bag cardigan authentic, paleo plaid Truffaut slow-carb. Hoodie plaid McSweeney's letterpress polaroid, Thundercats Truffaut locavore jean shorts next level deep v Godard farm-to-table occupy.</p>
+        <p>Although Reykjavik is the world’s northernmost capital, Iceland is warmed by the Gulf Stream, resulting in a surprisingly mild, coastal climate. The average summer temperature in Reykjavik is 10.6°C/51°F in July, with average highs of 24.3°C/76°F. The warmest months are from June to September when visitors can witness the midnight sun. Alternately, Iceland’s coldest months are November to January, when average winter temperature in Reykjavik are similar to New York City’s, about 0°C/32°F in January (average highs are 9.9°C/50°F).</p>
         <div class="forecast">
           <iframe id="forecast_embed" type="text/html" frameborder="0" height="245" width="100%" src="http://forecast.io/embed/#lat=64.1474&lon=-21.9340&name=Reykjavík&units=uk&color=#00CD9B"> </iframe>
         </div>
@@ -28,7 +33,7 @@
 
       <article id="accommodation">
         <h1>Accommodation while you're here</h1>
-        <p>Typewriter drinking vinegar narwhal cardigan. Forage seitan chia craft beer tattooed YOLO viral, bicycle rights kale chips. Tilde roof party DIY Truffaut, gluten-free pickled wayfarers beard readymade banh mi locavore Austin chia selfies. Mumblecore YOLO mlkshk salvia, single-origin coffee fashion axe leggings selfies swag sartorial Bushwick Pinterest Truffaut trust fund Schlitz. Put a bird on it PBR&B YOLO bespoke cliche, Thundercats swag mlkshk locavore polaroid next level fap. Messenger bag cardigan authentic, paleo plaid Truffaut slow-carb. Hoodie plaid McSweeney's letterpress polaroid, Thundercats Truffaut locavore jean shorts next level deep v Godard farm-to-table occupy.</p>
+        <p>We encourage our guests to get in touch with our <a href="/project1/hosts.php">hosts</a> in order to secure acommodation and pricing, however, we know this isn't always the easiest! So we will do our best to communicate with our hosts on your behalf if you are unable.</p>
       </article>
 
       <article id="testimonials">
