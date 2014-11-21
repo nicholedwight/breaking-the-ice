da9ea8afe444c8703f58c904d3e5ddb3c4c163be
Nichole Dwight <nicholedwight@gmail.com>
2014-11-21 16:09:59 +0000

    making reviews into the same style as testimonials

--- hostfamilies/Vilhjalmsson.php
+++ hostfamilies/Vilhjalmsson.php
@@ -8,7 +8,7 @@
 <main role="main" class="host_profile_main_wrapper">
   <div class="host_profile">
     <img src="/project1/assets/img/vil_profile.jpg" alt="Rut" class="host_profile_image">
-    <h2 class="host_name">Rut</h2>
+    <h2 class="host_name">Rut Vilhjalmsson</h2>
 
     <article class="listing_details">
       <h3>About Rut</h3>
@@ -16,28 +16,28 @@
       <p>We love travelling, meeting new people, outdoor activities, surfing, skiing, yoga, and lots of other things.</p>
       <p>For the past 5 years we´ve lived in Dubai, Scotland, and England and travelled alot, and recently moved back home to our gorgeous island.
         We're looking forward to being your host and will do our absolute best to make you feel comfortable and happy :)</p>
-      <a href="#"><div class="button">Request Booking</div></a>
-
+      <a href="/project1/contact"><div class="button">Request Booking</div></a>
     </article>
 
     <div class="review_wrapper">
+      <h3>Reviews</h3>
       <div class="review_item">
-        <img src="/project1/assets/img/jegtnes.jpg" alt="comment image" class="comment_image">
-        <h3 class="comment_user">Henrik Stoltz</h3>
-        <p class="comment_content">Utrolig sentralt, rent, pent og ryddig. Enkelt å finne frem til. 5min å gå fra sentrum. Billig parkering i gata utenfor og alltid ledig plass til bil. Kjempehyggelig vert og enkel å kommunisere med. Anbefales!</p>
-        <p class="comment_post_date">October 2014</p>
+        <h2 class="comment_user">Henrik Stoltz - </h2>
+        <blockquote class="comment_content">Utrolig sentralt, rent, pent og ryddig. Enkelt å finne frem til. 5min å gå fra sentrum. Billig parkering i gata utenfor og alltid ledig plass til bil. Kjempehyggelig vert og enkel å kommunisere med. Anbefales!<br>
+          <p class="comment_post_date">October 2014</p>
+        </blockquote>
       </div>
       <div class="review_item">
-        <img src="/project1/assets/img/jegtnes.jpg" alt="comment image" class="comment_image">
-        <h3 class="comment_user">Bobby</h3>
-        <p class="comment_content">He was a very nice and considerate host. He was extremely responsive, and was really understanding when we were accidentally a little late leaving in the morning. The place is very nice, clean, and modern.</p>
-        <p class="comment_post_date">October 2014</p>
+        <h2 class="comment_user">Bobby</h2>
+        <blockquote class="comment_content">He was a very nice and considerate host. He was extremely responsive, and was really understanding when we were accidentally a little late leaving in the morning. The place is very nice, clean, and modern.<br>
+          <p class="comment_post_date">October 2014</p>
+        </blockquote>
       </div>
       <div class="review_item">
-        <img src="/project1/assets/img/jegtnes.jpg" alt="comment image" class="comment_image">
-        <h3 class="comment_user">Henrik Stoltz</h3>
-        <p class="comment_content">Feel like home! Lovely house, adequate space, helpful and thoughtful host, great surf scene, ocean-side located, with everything we need. Highly recommended!!!</p>
-        <p class="comment_post_date">September 2014</p>
+        <h2 class="comment_user">Henrik Stoltz</h2>
+        <blockquote class="comment_content">Feel like home! Lovely house, adequate space, helpful and thoughtful host, great surf scene, ocean-side located, with everything we need. Highly recommended!!!<br>
+          <p class="comment_post_date">September 2014</p>
+        </blockquote>
       </div>
     </div>
   </div>
