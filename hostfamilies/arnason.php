12b39a0e0579a59e5fe08dde7423306362536674
Nichole Dwight <nicholedwight@gmail.com>
2014-11-22 20:16:47 +0000

    fixed layout on all host pages

--- hostfamilies/arnason.php
+++ hostfamilies/arnason.php
@@ -8,53 +8,60 @@
 </a>
 <main role="main" class="host_profile_main_wrapper">
   <div class="host_profile">
-    <img src="/project1/assets/img/arn_profile.jpg" alt="Gunnar" class="host_profile_image">
-    <h2 class="host_name">Gunnar</h2>
+    <div class="host_details cf">
+      <div class="name_container">
+        <img src="/project1/assets/img/arn_profile.jpg" alt="Gunnar" class="host_profile_image">
+        <h2 class="host_name">Gunnar</h2>
+      </div>
 
-    <article>
-      <h3>About Gunnary</h3>
-      <p>Hi im Gunnar!Reykjanesbær</p>
-      <p>I'm a fun guy from the Land of Fire and Ice, Iceland. I am also a professor in Icelandic language and literature - and a professional tourist guide, specialized in geology, history and culture. Beside running the guesthouse and welcoming my guests, I offer private excursion to the ocean for surf trips in a private vehicle for up to 8 passengers.</p>
-    <a href="#"><div class="button">Request Booking</div></a>
-    </article>
-
-    <div class="review_wrapper">
-      <div class="review_item">
-        <blockquote class="comment_content">Utrolig sentralt, rent, pent og ryddig. Enkelt å finne frem til. 5min å gå fra sentrum. Billig parkering i gata utenfor og alltid ledig plass til bil. Kjempehyggelig vert og enkel å kommunisere med. Anbefales!<br>
-          <p class="comment_post_date">Henrik Stoltz - October 2014</p>
-        </blockquote>
-      </div>
-      <div class="review_item">
-        <blockquote class="comment_content">He was a very nice and considerate host. He was extremely responsive, and was really understanding when we were accidentally a little late leaving in the morning. The place is very nice, clean, and modern.<br>
-          <p class="comment_post_date">Bobby - October 2014</p>
-        </blockquote>
-      </div>
-      <div class="review_item">
-        <blockquote class="comment_content">Feel like home! Lovely house, adequate space, helpful and thoughtful host, great surf scene, ocean-side located, with everything we need. Highly recommended!!!<br>
-          <p class="comment_post_date">Ana Biagi - September 2014</p>
-        </blockquote>
-      </div>
+      <article class="about_host">
+        <h3>About Gunnary</h3>
+        <p>Hi im Gunnar!Reykjanesbær</p>
+        <p>I'm a fun guy from the Land of Fire and Ice, Iceland. I am also a professor in Icelandic language and literature - and a professional tourist guide, specialized in geology, history and culture. Beside running the guesthouse and welcoming my guests, I offer private excursion to the ocean for surf trips in a private vehicle for up to 8 passengers.</p>
+      <a href="#"><div class="button">Request Booking</div></a>
+      </article>
     </div>
 
-    <aside class="listing_gallery">
-      <ul class="list_neutral">
-        <li>
-          <a href="/project1/assets/img/galleries/arn2.jpg" data-lightbox="arnason">
-            <img src="/project1/assets/img/galleries/arn2_thumb.jpg">
-          </a>
-        </li>
-        <li>
-          <a href="/project1/assets/img/galleries/arn3.jpg" data-lightbox="arnason">
-            <img src="/project1/assets/img/galleries/arn3_thumb.jpg">
-          </a>
-        </li>
-        <li>
-          <a href="/project1/assets/img/galleries/arn4.jpg" data-lightbox="arnason">
-            <img src="/project1/assets/img/galleries/arn4_thumb.jpg">
-          </a>
-        </li>
-      </ul>
-    </aside>
+    <div class="host_details cf">
+      <div class="review_wrapper">
+        <div class="review_item">
+          <blockquote class="comment_content">Utrolig sentralt, rent, pent og ryddig. Enkelt å finne frem til. 5min å gå fra sentrum. Billig parkering i gata utenfor og alltid ledig plass til bil. Kjempehyggelig vert og enkel å kommunisere med. Anbefales!<br>
+            <p class="comment_post_date">Henrik Stoltz - October 2014</p>
+          </blockquote>
+        </div>
+        <div class="review_item">
+          <blockquote class="comment_content">He was a very nice and considerate host. He was extremely responsive, and was really understanding when we were accidentally a little late leaving in the morning. The place is very nice, clean, and modern.<br>
+            <p class="comment_post_date">Bobby - October 2014</p>
+          </blockquote>
+        </div>
+        <div class="review_item">
+          <blockquote class="comment_content">Feel like home! Lovely house, adequate space, helpful and thoughtful host, great surf scene, ocean-side located, with everything we need. Highly recommended!!!<br>
+            <p class="comment_post_date">Ana Biagi - September 2014</p>
+          </blockquote>
+        </div>
+      </div>
+
+      <aside class="listing_gallery">
+        <h3>Gallery</h3>
+        <ul class="list_neutral">
+          <li>
+            <a href="/project1/assets/img/galleries/arn2.jpg" data-lightbox="arnason">
+              <img src="/project1/assets/img/galleries/arn2_thumb.jpg">
+            </a>
+          </li>
+          <li>
+            <a href="/project1/assets/img/galleries/arn3.jpg" data-lightbox="arnason">
+              <img src="/project1/assets/img/galleries/arn3_thumb.jpg">
+            </a>
+          </li>
+          <li>
+            <a href="/project1/assets/img/galleries/arn4.jpg" data-lightbox="arnason">
+              <img src="/project1/assets/img/galleries/arn4_thumb.jpg">
+            </a>
+          </li>
+        </ul>
+      </aside>
+    </div>
   </div>
 </main>
 
--- hostfamilies/jegtnes.php
+++ hostfamilies/jegtnes.php
@@ -8,56 +8,63 @@
 </a>
 <main role="main" class="host_profile_main_wrapper">
   <div class="host_profile">
-    <img src="/project1/assets/img/jegtnes.jpg" alt="Alex" class="host_profile_image">
-    <h2 class="host_name">Alex</h2>
+    <div class="host_details cf">
+      <div class="name_container">
+        <img src="/project1/assets/img/jegtnes.jpg" alt="Alex" class="host_profile_image">
+        <h2 class="host_name">Alex</h2>
+      </div>
 
-    <article>
-      <h3>About Alex</h3>
-      <p>Hi im Rut, and my husband's Ole!</p>
-      <p>We love travelling, meeting new people, outdoor activities, surfing, skiing, yoga, and lots of other things.</p>
-      <p>For the past 5 years we´ve lived in Dubai, Scotland, and England and travelled alot, and recently moved back home to our gorgeous island.
-        We're looking forward to being your host and will do our absolute best to make you feel comfortable and happy :)</p>
-      <a href="#"><div class="button">Request Booking</div></a>
-    </article>
-
-    <div class="review_wrapper">
-      <h3>Reviews</h3>
-      <div class="review_item">
-        <blockquote class="comment_content">Utrolig sentralt, rent, pent og ryddig. Enkelt å finne frem til. 5min å gå fra sentrum. Billig parkering i gata utenfor og alltid ledig plass til bil. Kjempehyggelig vert og enkel å kommunisere med. Anbefales!<br>
-          <p class="comment_post_date">Henrik Stoltz - October 2014</p>
-        </blockquote>
-      </div>
-      <div class="review_item">
-        <blockquote class="comment_content">He was a very nice and considerate host. He was extremely responsive, and was really understanding when we were accidentally a little late leaving in the morning. The place is very nice, clean, and modern.<br>
-          <p class="comment_post_date">Bobby - October 2014</p>
-        </blockquote>
-      </div>
-      <div class="review_item">
-        <blockquote class="comment_content">Feel like home! Lovely house, adequate space, helpful and thoughtful host, great surf scene, ocean-side located, with everything we need. Highly recommended!!!<br>
-          <p class="comment_post_date">Ana Biagi - September 2014</p>
-        </blockquote>
-      </div>
+      <article class="about_host">
+        <h3>About Alex</h3>
+        <p>Hi im Rut, and my husband's Ole!</p>
+        <p>We love travelling, meeting new people, outdoor activities, surfing, skiing, yoga, and lots of other things.</p>
+        <p>For the past 5 years we´ve lived in Dubai, Scotland, and England and travelled alot, and recently moved back home to our gorgeous island.
+          We're looking forward to being your host and will do our absolute best to make you feel comfortable and happy :)</p>
+        <a href="#"><div class="button">Request Booking</div></a>
+      </article>
     </div>
 
-    <aside class="listing_gallery">
-      <ul class="list_neutral">
-        <li>
-          <a href="/project1/assets/img/galleries/jeg2.jpg" data-lightbox="jegtnes">
-            <img src="/project1/assets/img/galleries/jeg2_thumb.jpg">
-          </a>
-        </li>
-        <li>
-          <a href="/project1/assets/img/galleries/jeg3.jpg" data-lightbox="jegtnes">
-            <img src="/project1/assets/img/galleries/jeg3_thumb.jpg">
-          </a>
-        </li>
-        <li>
-          <a href="/project1/assets/img/galleries/jeg4.jpg" data-lightbox="jegtnes">
-            <img src="/project1/assets/img/galleries/jeg4_thumb.jpg">
-          </a>
-        </li>
-      </ul>
-    </aside>
+    <div class="host_details cf">
+      <div class="review_wrapper">
+        <h3>Reviews</h3>
+        <div class="review_item">
+          <blockquote class="comment_content">Utrolig sentralt, rent, pent og ryddig. Enkelt å finne frem til. 5min å gå fra sentrum. Billig parkering i gata utenfor og alltid ledig plass til bil. Kjempehyggelig vert og enkel å kommunisere med. Anbefales!<br>
+            <p class="comment_post_date">Henrik Stoltz - October 2014</p>
+          </blockquote>
+        </div>
+        <div class="review_item">
+          <blockquote class="comment_content">He was a very nice and considerate host. He was extremely responsive, and was really understanding when we were accidentally a little late leaving in the morning. The place is very nice, clean, and modern.<br>
+            <p class="comment_post_date">Bobby - October 2014</p>
+          </blockquote>
+        </div>
+        <div class="review_item">
+          <blockquote class="comment_content">Feel like home! Lovely house, adequate space, helpful and thoughtful host, great surf scene, ocean-side located, with everything we need. Highly recommended!!!<br>
+            <p class="comment_post_date">Ana Biagi - September 2014</p>
+          </blockquote>
+        </div>
+      </div>
+
+      <aside class="listing_gallery">
+        <h3>Gallery</h3>
+        <ul class="list_neutral">
+          <li>
+            <a href="/project1/assets/img/galleries/jeg2.jpg" data-lightbox="jegtnes">
+              <img src="/project1/assets/img/galleries/jeg2.jpg">
+            </a>
+          </li>
+          <li>
+            <a href="/project1/assets/img/galleries/jeg3.jpg" data-lightbox="jegtnes">
+              <img src="/project1/assets/img/galleries/jeg3.jpg">
+            </a>
+          </li>
+          <li>
+            <a href="/project1/assets/img/galleries/jeg4.jpg" data-lightbox="jegtnes">
+              <img src="/project1/assets/img/galleries/jeg4.jpg">
+            </a>
+          </li>
+        </ul>
+      </aside>
+    </div>
   </div>
 </main>
 
