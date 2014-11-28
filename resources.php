bae9459ecbd84ba05d9d5957a2fbce1fc0b9d1c7
Nichole Dwight <nicholedwight@gmail.com>
2014-11-28 13:22:00 +0000

    resources page, my pinky hurts

---
+++ resources.php
@@ -0,0 +1,87 @@
+<?php $thisPage = "Hosts";
+  include('inc/header.php');
+?>
+<div class="resources_wrapper">
+  <h2>Image Sources</h2>
+
+  <p><a href="http://instagram.com/p/ramwkYxyJe">arn_profile.jpg</a></p>
+
+  <p><a href="https://www.airbnb.co.uk/rooms/451155">arn1.jpg/arn2.jpg/arn4.jpg/arn5.jpg</a></p>
+
+  <p><a href="#">arn3.jpg -****</a></p>
+
+  <p><a href="https://www.airbnb.co.uk/rooms/588487">ekl1.jpg/ekl2.jpg/ekl4.jpg/ekl5.jpg</a></p>
+
+  <p><a href="https://www.flickr.com/photos/ericakohn/5196107013/">ekl3.jpg</a></p>
+
+  <p><a href="https://www.flickr.com/people/donchris/photosof/page3/">ekl_profile.jpg</a></p>
+
+  <p><a href="http://hudsonvalleypropertygroup.com/airbnb-a-company-for-the-people/">jeg1.jpg</a></p>
+
+  <p><a href="https://www.flickr.com/photos/hownowdesign/2196875058/in/photolist-4m8yqb-9wabCn-aNixta-76cd5U-5WnF5n-7TYhfd-5W74G5-mg5U7H-99WpHJ-EQb7G-4G1PUG-418YYQ-3BMZgm-9QzJ8j-5WnjRZ-6vnp3X-7oRnAG-Ke7My-2Vtkks-9bo6Lk-3YwyT-nfDRYL-abfFi5-bm8RDy-bz3KSP-dgzQ3s-boALRb-5ZhGqs-4F6u9J-dSsPkg-9brenj-5XUNf-6Cn5gv-4yRZX9-mAStrV-4yRZX5-dBMwjn-aaM8fk-abfFeN-4ErvDf-4FWDEZ-4G1PQG-fwDr5c-3YwrE-mewZur-5jn87e-6w5SKq-54yVYM-7vheaF-7XnbW">jeg2.jpg</a></p>
+
+  <p><a href="https://www.flickr.com/photos/chalonuk/6207312254/">jeg3.jpg</a></p>
+
+  <p><a href="https://www.airbnb.co.uk/rooms/2451466">jeg4.jpg</a></p>
+
+  <p><a href="https://www.flickr.com/photos/taivasalla/1590469109/in/photolist-kDZNJ7-Ds3A8-dBoNU4-Abwzm-ABdSL-4oewNo-3qxz4c-aXMb4p-Fp6HQ-BUCsH-bodPZA-9Kgk93-ajtR16-3qC77S-7BzLw-5tnSi9-fsHrUc-7BzZQ-d7CwtC-7BA1j-2adXnH-4qpKNM-kTKWAy-5fgx-aNpidi-kVVFLv-4F844J-3RBKRo-5mZ3s4-5kfzaE-KJ3i9-5mZ1rc-4Gnobu-4Gie4T-hwk4yT-7Svjux-5bzuRh-7wvEYv-5wkP5G-dP8q5q-8eUF8p-9RPwSB-e2GAxc-3F9vP4-cUJXXo-3jbyf3-3BAYsB-d8i8bL-3nsJDk-e3bMFg">jeg5.jpg</a></p>
+
+  <p><a href="https://www.flickr.com/photos/h_is_for_home/4429986692/in/faves-pirik/">osk1.jpg</a></p>
+
+  <p><a href="https://www.airbnb.co.uk/rooms/235358?checkin=28-11-2014&checkout=29-11-2014&s=g7sy">osk2.jpg/osk4.jpg</a></p>
+
+  <p><a href="https://www.airbnb.co.uk/rooms/860028">osk3.jpg/osk5.jpg</a></p>
+
+  <p><a href="https://www.flickr.com/photos/dalli/2893557462/">ron1.jpg</a></p>
+
+  <p><a href="http://www.yusrablog.com/inspiration/kitchen-decorating-pictures-ideas/">ron2.png</a></p>
+
+  <p><a href="https://www.flickr.com/photos/rogueinteriordesigns/4843222591/in/gallery-floringorgan-72157627078900178/">ron3.jpg</a></p>
+
+  <p><a href="http://www.indoema.com/interior-designs-of-modern-house-ideas/">ron4.jpg</a></p>
+
+  <p><a href="https://www.flickr.com/photos/stuckincustoms/5814636567/in/photolist-9RPwSB-e2GAxc-3F9vP4-cUJXXo-3jbyf3-3BAYsB-d8i8bL-3nsJDk-e3bMFg-azzhMU-4EBcw9-4FdJ2f-8WpZr5-5ePbjx-dWPwC7-5myyYX-eh74S-B7566-a4p3tx-5zg12N-5LRMKW-L99bg-PFpWs-8GZN9K-3nTJyG-bddRMK-61pX16-cpS8no-7WwcLr-8WzTfa-8Wm81i-4iTrcV-3djS6o-pcfLYh-5KE9cK-pcgoXh-cNoxZf-8WzRhZ-ebPDee-ft9SDM-aznjqE-aC1huw-dLBCyw-3bMWfw-3jby8A-5kbcBv-7yqY41-3jby5o-3cWknJ-4EAZoN">ron5.jpg</a></p>
+
+  <p><a href="http://www.skybrotherindia.com/works.html">vil1.png</a></p>
+
+  <p><a href="https://www.flickr.com/photos/rogueinteriordesigns/4843175239/">vil2.jpg</a></p>
+
+  <p><a href="https://www.pinterest.com/pin/327636941611763052/">vil3.jpg</a></p>
+
+  <p><a href="http://www.yusrablog.com/inspiration/kitchen-decorating-pictures-ideas/">vil4.jpg</a></p>
+
+  <p><a href="#">vil5.png - *****</a></p>
+
+  <p><a href="http://www.thefourohfive.com/news/article/watch-our-trailer-for-tonlist-the-405-icelandic-music-documentary-141">bg.jpg</a></p>
+
+  <p><a href="http://weheartit.com/entry/147160437/via/angelina_speer">blanketgirl.jpg</a></p>
+
+  <p><a href="https://www.pinterest.com/pin/16747829834574305/">bonfire.jpg</a></p>
+
+  <p><a href="http://www.surfingmagazine.com/magazine/march-issue-outtakes-iceland/">contactbg.jpg</a></p>
+
+  <p><a href="https://unsplash.com/paulozono">hostbg.jpg</a></p>
+
+  <p>iceball.jpg - I created this in photoshop</p>
+
+  <p><a href="http://www.panoramio.com/photo/4840629">icelandhills.jpg</a></p>
+
+  <p>jegtnes.jpg - Photo I own</p>
+
+  <p><a href="http://www.inspiredbyiceland.com/">mappoints.png</a></p>
+
+  <p><a href="https://www.flickr.com/photos/aigle_dore/9687226902/in/photolist-fFLmXX-mRuS5p-getV1y-gt8m7o-ggdbiB-gett4S-getDiR-fL2z4u-9eoZ5C-getFvs-getEgS-fSxZGz-nuUSuA-n16bez-getDaf-3zuk9r-nuURRo-getmJZ-getran-fE7eTZ-getSHh-cjzaoQ-cjzaVq-cjzaU1-am2YBV-cjzaSs-cjzaLW-cjza2C-cjzawo-mN8HBn-geudsp-nKmSnU-nuUR8C-PRU1s-PSiXB-PRTZL-PRUHU-getsG9-nKmSBm-nuUQM7-3zuDnn-6oEqaa-cjzaNy-iTB7c1-am2Xdz-e7ZYkj-PRU1W-gfXUVR-PRHuy-am2Z46">northernlights.jpg</a></p>
+
+  <p><a href="https://www.flickr.com/photos/bombtwinz/6474411085/in/set-72157628326779043">osk_profile.jpg</a></p>
+
+  <p><a href="https://plus.google.com/117267217174213629320/photos">ron_profile.jpg</a></p>
+
+  <p><a href="http://rebloggy.com/post/love-pretty-girl-cool-music-beautiful-perfect-hippie-hipster-vintage-landscape-b/82275721849">sea.jpg</a></p>
+
+  <p><a href="http://blog.aaa-trans.com/limousine/holidays/">vil_profile.jpg</a></p>
+
+  <p><a href="https://www.flickr.com/photos/skim_michigan/8006623225/">waves.png</a></p>
+
+</div>
+
+<?php include('inc/footer.php'); ?>
